770dc686804266b6bce3fe0bf29a163ccb2fef58
Hoang Phuc <49684127+Huangphoux@users.noreply.github.com>
2025-09-21 17:15:36 +0700

    Minor

--- install.sh
+++ install.sh
@@ -1,9 +1,17 @@
 #! /bin/bash
+
+# Install concurrently
 npm install
+
+# Install dependencies for server
 cd server || exit
 npm install
+
+# Install dependencies for client
 cd ..
 cd client || exit
 npm install
+
+# Start localhost
 cd ..
 npm run dev
