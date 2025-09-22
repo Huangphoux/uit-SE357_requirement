d3762496169461cc88c389ce97a65a6a0e9917ed
Hoang Phuc <49684127+Huangphoux@users.noreply.github.com>
2025-09-22 19:03:32 +0700

    Add Github Actions workflow

--- server/index.js
+++ server/index.js
@@ -1,16 +1,16 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello, world!"));
-
 const PORT = process.env.PORT || 3000;
 
+app.get("/", (req, res) => {
+  res.send("Hello, world!");
+});
+
 app.listen(PORT, (error) => {
-  // This is important!
-  // Without this, any startup errors will silently fail
-  // instead of giving you a helpful error message.
   if (error) {
     throw error;
   }
+
   console.log(`My first Express app - listening on port ${PORT}!`);
 });
